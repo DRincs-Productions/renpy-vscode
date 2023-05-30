55db6f4c1386cccf7fbbbef0c414a86327f462ec
Exodia <67595890+DonRP@users.noreply.github.com>
2023-05-30 13:00:28 +0200

    varius implement

--- src/extension.ts
+++ src/extension.ts
@@ -15,25 +15,31 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log("Ren'Py VSCode extension activated!");
 
 	let settings = vscode.workspace.getConfiguration('renpy-vscode');
+	let renpySDKPath: string | undefined = settings.get("renpy.renpySDKPath");
 	
 	// import python extension
 	let python = extensions.getExtension('ms-python.python');
-	let pythonApi = python?.exports;
-	// import renpy-languague extension
-	let renpyLanguage = extensions.getExtension('luquedaniel.languague-renpy');
-	let renpyLanguageApi = renpyLanguage?.exports;
-
-	let renpySDKPath: string | undefined = settings.get("renpy.renpySDKPath");
-	if (!renpySDKPath)
+	if (!python)
 	{
-		vscode.window.showErrorMessage("The setting 'renpy.renpySDKPath' has not been set");
+		vscode.window.showErrorMessage("The 'ms-python.python' extension is required");
 	}
 	else
 	{
-		// Add a path to the Python environment
-		pythonApi.setExecutionDetails('python', { path: renpySDKPath });
+		let pythonApi = python?.exports;
+		if (!renpySDKPath)
+		{
+			vscode.window.showErrorMessage("The setting 'renpy.renpySDKPath' has not been set");
+		}
+		else
+		{
+			// Add a path to the Python environment
+			pythonApi.setExecutionDetails('python', { path: renpySDKPath });
+		}
 	}
 
+	// import renpy-languague extension
+	let renpyLanguage = extensions.getExtension('luquedaniel.languague-renpy');
+	let renpyLanguageApi = renpyLanguage?.exports;
 }
 
 // This method is called when your extension is deactivated
